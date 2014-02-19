35194ffffe20311523d3fb482aa37f08ed258409
jhdk <jihuang@live.cn>
2014-02-19 12:55:51 +0100

    first

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School
+{
+    class Program
+    {
+        private String name;
+        private int age;
+
+        public Program()
+        {
+        }
+
+        public Program(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School
+{
+    class Student  
+    {
+        private String _name;
+        private int _age;
+        public String Address { get; set; }
+        public Student(String name, int age){
+            this._name = name;
+            this._age = age;
+
+        }
+
+        public Student(string name, int age, string address)
+        {
+            _name = name;
+            _age = age;
+            Address = address;
+        }
+
+        public string toString()
+        {
+            
+        }
+    }
+
+
+}
